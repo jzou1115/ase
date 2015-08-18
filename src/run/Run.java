d8ccbd931e94d8dd4b2b1967895868a5e6bf79bd
jzou1115 <jzou1115@gmail.com>
2015-08-18 09:28:13 -0700

    change Run class

--- src/run/Run.java
+++ src/run/Run.java
@@ -1,5 +1,10 @@
 package run;
 
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -31,17 +36,16 @@ public class Run {
 	public int mapASE(int[] ase){
 		int variants=0;
 		for(SNP s:snps){
-			ArrayList<GenoSample> gsamples = s.getGenosamples();
+			List<GenoSample> gsamples = s.getGenosamples();
 			int correct=0;
 			int incorrect=0;
-			for (GenoSample g : gsamples) {
-				String sampleID = g.getSampleID();
-				int isHetero = g.getHetero();
-				randInt = rand.nextInt(2);
-				if(isHetero == randInt){
+			for (int i=0; i<gsamples.size();i++) {
+				String sampleID = gsamples.get(i).getID();
+				int isHetero = gsamples.get(i).getHetero();
+				if(isHetero == ase[i]){
 					correct++;
 				}
-				else if(isHetero != randInt){
+				else if(isHetero != ase[i]){
 					incorrect++;
 				}
 				else{
@@ -56,16 +60,71 @@ public class Run {
 		return variants;
 	}
 	
-	public int[] permute(int[] ase){
+	public int[] permute(int[] ase, int n){
+		int a[] = new int[n];
+		int ind[] = new int[n];
 		
+		for(int i=0;i<n;i++){
+			ind[i] =0;
+		}
+		int index;
+		Random rand = new Random();
+		for(int i=0; i<n;i++){
+			do{
+				index = rand.nextInt(n);
+			} while(ind[index] != 0);
+			ind[index] = 1;
+			a[i]= ase[index];
+		}
+		return a;
 	}
 	
-	public double simulate(){
-		
+	public double simulate(SNP s, int[] ase) throws FileNotFoundException{
+		int total=0;
+		for(int i=0; i<perm;i++){
+			int[] p = permute(ase, ase.length);
+			int a = mapASE(p);
+			total=total+a;
+		}
+		return 1.0*total/perm;
 	}
 	
+	public double calculateMAF(SNP s){
+		int hetero=0;
+		
+		List<GenoSample> genos = s.getGenosamples();
+		for(int i=0; i<genos.size();i++){
+			if(genos.get(i).getHetero()==1){
+				hetero++;
+			}
+		}
+		if(hetero>=genos.size()){
+			return 1.0*(genos.size()-hetero)/genos.size();
+		}
+		return 1.0*hetero/genos.size();
+	}
 	
-	//TODO think about when correct AND incorrect can be 0, because this happened before
+	public int[] aseCall(SNP s){
+		List<GenoSample> genos = s.getGenosamples();
+		int[] ret = new int[genos.size()];
+		for(int i=0; i<genos.size();i++){
+			ret[i] = genos.get(i).getHetero();
+		}
+		return ret;
+	}
+	
+	public void allSimulations() throws IOException{
+		BufferedWriter outfile = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(gene.getId()+"_simulation.txt")));
+		outfile.write("GeneID\tMAF\tNumVariants\tSimulationMean\n");
+		for(SNP s: snps){
+			double f = calculateMAF(s);
+			int[] ase = aseCall(s);
+			double mean = simulate(s, ase);		
+			int x= mapASE(ase);
+			outfile.write(s.getId()+"\t"+f+"\t"+x+"\t"+mean+"\n");
+		}
+	}
+	
 	public boolean passThreshold(int incorrect, int correct){
 		if (correct == 0) {
 			return false;
