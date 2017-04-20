6029006b351878ce0ba655765670446432bfa040
jzou1115 <jzou1115@gmail.com>
2017-04-20 12:57:22 -0700

    incorporate phase into ParseExpressions

--- src/parse/ParseExpressions.java
+++ src/parse/ParseExpressions.java
@@ -14,12 +14,12 @@ import genome.Gene;
 import sample.ExpSample;
 
 public class ParseExpressions {
-	
+
 	public static List<String> parseExpressions(InputStream expressions, Gene g, File outdir) throws IOException{
 		System.out.println("Reading expressions");
 		BufferedReader br = new BufferedReader(new InputStreamReader(expressions));
 		String line;
-		
+
 		Map<String, Integer> reads = new HashMap<String,Integer>(); //Map GTEx id of individual to total number of reads for individual
 		Map<String, Integer> ref = new HashMap<String,Integer>(); //Map GTEx id of individual to number of reference reads for individual
 		List<String> ret = new ArrayList<String>();
@@ -29,21 +29,33 @@ public class ParseExpressions {
 				try{
 					String[] tokens = line.split("\\s+");
 					String gtexid = tokens[6].trim();
-					ret.add(gtexid);
+
 					int refAllele = Integer.parseInt(tokens[8]);
+					int altAllele = Integer.parseInt(tokens[9]);
 					int totalReads = Integer.parseInt(tokens[10]);
-					
-					//initialize individual to have 0 reference reads and 0 total reads
-					if(!reads.containsKey(gtexid)){
-						reads.put(gtexid, 0);
-						ref.put(gtexid, 0);
+					boolean isLeft = isLeft(tokens[18]);
+
+					if(totalReads>=10){
+						ret.add(gtexid);
+						//initialize individual to have 0 reference reads and 0 total reads
+						if(!reads.containsKey(gtexid)){
+							reads.put(gtexid, 0);
+							ref.put(gtexid, 0);
+						}
+
+						//Update read count
+						if(isLeft){
+							int refKey = ref.get(gtexid) + altAllele;
+							ref.put(gtexid, refKey);
+						}
+						else{
+							int refKey = ref.get(gtexid) + refAllele;
+							ref.put(gtexid, refKey);
+						}
+
+						int readsKey = reads.get(gtexid) + totalReads;
+						reads.put(gtexid, readsKey);	
 					}
-					
-					//Update read count
-					int refKey = ref.get(gtexid) + refAllele;
-					int readsKey = reads.get(gtexid) + totalReads;
-					ref.put(gtexid, refKey);
-					reads.put(gtexid, readsKey);
 
 				} catch (Exception e){
 					e.printStackTrace();
@@ -61,42 +73,64 @@ public class ParseExpressions {
 
 			//determine whether each individual has ASE or not
 			//BufferedWriter outfile = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(outdir+File.separator+g.getId()+"_ase.txt")));		
-			
-			int totalASE=0;
+
+			//int totalASE=0;
 			for(String sample: reads.keySet()){
 				ExpSample expsamp;
 				double allelicRatio = 1.0*ref.get(sample) / reads.get(sample);
-				int hasASE;
-				
+				//int hasASE;
+
 				if(allelicRatio>0.65){
 					expsamp = new ExpSample(sample, 1);
-					hasASE=1;
-					totalASE++;
+					//hasASE=1;
+					//totalASE++;
 				}
 				else if(allelicRatio<0.35){
 					expsamp = new ExpSample(sample, 1);
-					hasASE=1;
-					totalASE++;
+					//hasASE=1;
+					//totalASE++;
 				}
 				else{
 					expsamp = new ExpSample(sample, 0);
-					hasASE=0;
+					//hasASE=0;
 				}
 
 				g.addSample(expsamp);
-				
+
 				//outfile.write(sample+"\t"+allelicRatio+"\t"+hasASE+"\n");
-		
+
 			}
 			//outfile.write(g.getId()+"\t"+totalASE+"\t"+reads.size()+"\t"+totalASE*1.0/reads.size());
 			//outfile.close();
-		
+
 
 		} catch (IOException e) {
 			e.printStackTrace();
 			System.exit(1);
 		}
 		return ret;
-		
+
+	}
+
+	private static boolean isLeft(String string) {
+		String[] tokens = string.split(";");
+		String[] tokens2 = tokens[1].split("|");
+		if(!isInteger(string)){
+			System.exit(1);
+		}
+		if(Integer.parseInt(tokens2[0])==1){
+			return true;
+		}
+		return false;
+	}
+
+	public static boolean isInteger( String input ) {
+		try {
+			Integer.parseInt( input );
+			return true;
+		}
+		catch( Exception e ) {
+			return false;
+		}
 	}
 }
