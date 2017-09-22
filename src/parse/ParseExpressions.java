56c7b81a21e71a344f4e919dc061c1c4728c9a9e
jzou1115 <jzou1115@gmail.com>
2017-09-22 12:15:53 -0700

    change ASE call from threshold to binomial test

--- src/parse/ParseExpressions.java
+++ src/parse/ParseExpressions.java
@@ -27,7 +27,7 @@ public class ParseExpressions {
 		String line = br.readLine(); //skip header
 		
 		Map<String, Integer> reads = new HashMap<String,Integer>(); //Map GTEx id of individual to total number of reads for individual
-		Map<String, Integer> ref = new HashMap<String,Integer>(); //Map GTEx id of individual to number of reference reads for individual
+		Map<String, Integer> aseCall = new HashMap<String,Integer>(); //Map GTEx id of individual to ase call based on binomial test
 		List<String> ret = new ArrayList<String>();
 		try {
 			//iterate over lines in file (SNPs used to call ASE)
@@ -36,33 +36,23 @@ public class ParseExpressions {
 					String[] tokens = line.split("\\s+");
 					String gtexid = tokens[6].trim();
 					
-					int refAllele = Integer.parseInt(tokens[8]);
-					int altAllele = Integer.parseInt(tokens[9]);
+					double p = Double.parseDouble(tokens[15]);
 					int totalReads = Integer.parseInt(tokens[10]);
-					boolean isLeft = isLeft(tokens[18]);
-					//boolean isLeft=true;
+
+					if(!reads.containsKey(gtexid)){
+						reads.put(gtexid, 0);
+					}
 					
-					if(totalReads>=20){
-						ret.add(gtexid);
-						//initialize individual to have 0 reference reads and 0 total reads
-						if(!reads.containsKey(gtexid)){
-							reads.put(gtexid, 0);
-							ref.put(gtexid, 0);
-						}
-						
-						//Update read count
-						if(isLeft){
-							int refKey = ref.get(gtexid) + altAllele;
-							ref.put(gtexid, refKey);
+					if(totalReads > reads.get(gtexid)){
+						reads.put(gtexid, totalReads);
+						if(p<.05){
+							aseCall.put(gtexid, 1);
 						}
 						else{
-							int refKey = ref.get(gtexid) + refAllele;
-							ref.put(gtexid, refKey);
+							aseCall.put(gtexid, 0);
 						}
-						
-						int readsKey = reads.get(gtexid) + totalReads;
-						reads.put(gtexid, readsKey);	
 					}
+					
 
 				} catch (Exception e){
 					e.printStackTrace();
@@ -72,72 +62,26 @@ public class ParseExpressions {
 
 			br.close();
 
-			if(reads.size()!=ref.size()){
-				System.out.println("Number of samples with total reads does not match number of samples with reference reads");
-				System.exit(1);
-			}
-
 
 			//determine whether each individual has ASE or not
-			//BufferedWriter outfile = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(outdir+File.separator+g.getId()+"_ase.txt")));		
+			BufferedWriter outfile = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(outdir+File.separator+g.getId()+"_ase.txt")));		
 			
-			int totalASE=0;
-			for(String sample: reads.keySet()){
+			for(String sample: aseCall.keySet()){
 				ExpSample expsamp;
-				double allelicRatio = 1.0*ref.get(sample) / reads.get(sample);
-				int hasASE;
-				
-				if(allelicRatio>0.65){
-					expsamp = new ExpSample(sample, 1);
-					hasASE=1;
-					totalASE++;
-				}
-				else if(allelicRatio<0.35){
-					expsamp = new ExpSample(sample, 1);
-					hasASE=1;
-					totalASE++;
+				int call = aseCall.get(sample);
+				if(call==0){
+					expsamp = new ExpSample(sample, 0);
 				}
 				else{
-					expsamp = new ExpSample(sample, 0);
-					hasASE=0;
+					expsamp = new ExpSample(sample, 1);
 				}
-
 				g.addSample(expsamp);
 				
-				//outfile.write(sample+"\t"+allelicRatio+"\t"+hasASE+"\n");
+				outfile.write(sample+"\t"+call+"\n");
 		
 			}
-			//outfile.write(g.getId()+"\t"+totalASE+"\t"+reads.size()+"\t"+totalASE*1.0/reads.size());
-			//outfile.close();
+			outfile.close();
 		
-			
-			
-			//binomial test
-			/*int numSamples = reads.keySet().size();
-			double alpha = .05/numSamples;
-			
-			for(String sample: reads.keySet()){
-				int refReads = ref.get(sample);
-				int totalReads = reads.get(sample);
-				
-				if(totalReads>20){
-					BinomialTest bt = new BinomialTest();
-					boolean ase = bt.binomialTest(totalReads, refReads, .5, AlternativeHypothesis.TWO_SIDED, alpha);
-					
-					ExpSample expsamp;
-					if(ase){
-						expsamp = new ExpSample(sample, 1);
-					}
-					else{
-						expsamp = new ExpSample(sample, 0);
-					}
-					
-					g.addSample(expsamp);	
-				}
-				
-				//outfile.write(sample+"\t"+allelicRatio+"\t"+hasASE+"\n");	
-			}
-			*/	
 
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -147,26 +91,5 @@ public class ParseExpressions {
 		
 	}
 
-	private static boolean isLeft(String string) {
-		String[] tokens = string.split(";");
-		String[] tokens2 = tokens[1].split("|");
-		if(!isInteger(tokens2[0])){
-			System.out.println("Error: Cannot parse genotype "+string);
-			System.exit(1);
-		}
-		if(Integer.parseInt(tokens2[0])==1){
-			return true;
-		}
-		return false;
-	}
-	
-	public static boolean isInteger( String input ) {
-	    try {
-	        Integer.parseInt( input );
-	        return true;
-	    }
-	    catch( Exception e ) {
-	        return false;
-	    }
-	}
+
 }
